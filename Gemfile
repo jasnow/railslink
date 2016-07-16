2aaf6dbfa95a08e4852391dabdd4bcef618e77cb
Al Snow <jasnow@hotmail.com>
2016-07-16 10:09:45 -0400

    Upgraded repo to Rails 5.0.0

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'https://rubygems.org'
 
-gem 'rails', '4.2.7'
+gem 'rails', '5.0.0'
+gem 'rails-controller-testing'
 
 ruby '2.3.1'
 
--- config/application.rb
+++ config/application.rb
@@ -20,9 +20,6 @@ module Railslink
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
-    # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
-    
     # Global values
     configatron.app_name = 'Ruby on Rails Link'
     configatron.ga.tracking_id = 'UA-64301219-1'
