769a807f172d0212cfea5c4e93078f504e7f5943
Al Snow <jasnow@hotmail.com>
2016-07-17 10:02:20 -0400

    Upgraded to Ruby 2.4.0-preview1

--- Gemfile
+++ Gemfile
@@ -3,7 +3,7 @@ source 'https://rubygems.org'
 gem 'rails', '5.0.0'
 gem 'rails-controller-testing'
 
-ruby '2.3.1'
+ruby '2.4.0'
 
 gem 'puma'
 gem 'pg'
--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -22,9 +21,8 @@ ActiveRecord::Schema.define(version: 20150808121006) do
     t.jsonb    "response",    default: {}, null: false
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
+    t.index ["created_at"], name: "index_slack_api_responses_on_created_at", using: :btree
+    t.index ["updated_at"], name: "index_slack_api_responses_on_updated_at", using: :btree
   end
-
-  add_index "slack_api_responses", ["created_at"], name: "index_slack_api_responses_on_created_at", using: :btree
-  add_index "slack_api_responses", ["updated_at"], name: "index_slack_api_responses_on_updated_at", using: :btree
 
 end
